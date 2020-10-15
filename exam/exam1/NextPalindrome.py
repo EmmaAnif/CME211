ae041baa2faf3f78cd81001f5a58f311ba49bc2f
Emmanuel Anifowose <anifs@stanford.edu>
2020-10-15 02:01:46 -0700

    commit exam files

---
+++ exam/exam1/NextPalindrome.py
@@ -0,0 +1,29 @@
+"""My NextPalindrome Module"""
+
+def NextPalindrome(pal):
+    """Takes in a 5 digit string describing a Palindrome
+    Returns a string with the next 5 digit palindrome.
+    
+    defaults to "99999" if wrong value is given
+    """
+    nextPal = "99999" #defualt return value
+    
+    if int(pal[2]) < 9: #checks if the middle digit is 9
+        midPal = str(int(pal[2]) + 1) #update middle digit
+        nextPal = pal[:2] + midPal + pal[3:]
+        
+    elif int(pal[1]) < 9: #checks if the 2nd and 4th digit are 9s
+        midPal = str(0) #overflow of middle digit
+        leftMidPal = str(int(pal[1]) + 1)
+        rightMidPal = leftMidPal
+        nextPal = pal[0]+leftMidPal+midPal+rightMidPal+pal[-1] 
+        
+    elif int(pal[0]) < 9: #checks if the 1st and last digits are 9s
+        midPal = str(0) #overflow of middle digit
+        leftMidPal = str(0) #overflow of 2nd and 4th digit
+        leftPal = str(int(pal[0]) + 1)
+        rightMidPal = leftMidPal
+        rightPal = leftPal
+        nextPal = leftPal+leftMidPal+midPal+rightMidPal+rightPal
+        
+    return nextPal
---
+++ exam/exam1/README.md
@@ -0,0 +1,41 @@
+Language Choice
+1. Low level language because code needs to be optimized for performance
+2. Low level language because code needs to be optimized for performance
+3. High level language because it's easier to test and develop code with it
+4. High level language because code doesn not do a lot of computation
+5. Low level language because optimizing memory is key in embedded systems.
+
+Data Types
+1. Integer
+2. Floating point
+3. Floating point
+4. Integer
+5. Floating point
+
+Loops
+1. for
+2. for
+3. while
+4. for
+5. for
+
+Sequence
+1. tuple
+2. dictionary
+3. tuple
+4. list
+5. dictionary
+
+Conditionals
+- The conditonals are supposed to test for the same call of random.random()
+to get a correct distribution. However, random.random() is called twice 
+which doesn't give the expected distribution.
+A correct approach would have been to set testVal = random.random(). The 
+conditional should then be tested on testVal.
+
+Mutable or Immutable
+The weevil datatype is mutable since its elements can be modified as shown 
+in a[1] = 7 and b[-1] = "goodbye".
+
+
+
